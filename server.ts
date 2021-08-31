993503f576e8e5749edef90ecb4038745a9111c5
Akwasi Appiah <akwasi.atta.appiah@gmail.com>
2021-08-31 12:10:11 +0100

    Added prettier and try catch to routes

--- server.ts
+++ server.ts
@@ -11,8 +11,10 @@ config(); //Read .env file lines as though they were env vars.
 //For the ssl property of the DB connection config, use a value of...
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? { rejectUnauthorized: false } : herokuSSLSetting
+const herokuSSLSetting = { rejectUnauthorized: false };
+const sslSetting = process.env.LOCAL
+  ? { rejectUnauthorized: false }
+  : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
@@ -21,35 +23,42 @@ const dbConfig = {
 const app = express();
 
 app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
+app.use(cors()); //add CORS support to each following route handler
 
 const client = new Client(dbConfig);
 client.connect();
 
 app.get("/events", async (req, res) => {
-  
-  const dbres = await client.query('select * from events');
-  res.json(dbres.rows);
-
+  try {
+    const dbres = await client.query("select * from events");
+    res.json(dbres.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
-app.get("/event-info/:event_id", async(req,res) => {
-  
-  const { event_id } = req.params;
-  const dbres = await client.query('select attendee_name from event_info WHERE event_id = $1', [event_id]);
-  res.json(dbres.rows);
-})
+app.get("/event-info/:event_id", async (req, res) => {
+  try {
+    const { event_id } = req.params;
+    const dbres = await client.query(
+      "select attendee_name from event_info WHERE event_id = $1",
+      [event_id]
+    );
+    res.json(dbres.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // app.get("/event test", async(req,res) => {
 //   const dbres = await client.query('select * from event_info');
 //   res.json(dbres.rows);
 // })
 
-
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
